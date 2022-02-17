7e6ebceb1c70b002c5c457538b9ad13ca256122b
UijinJung <tkrk1226@ajou.ac.kr>
2022-02-17 16:01:58 +0900

    basics #2 [220217-1601]

--- javascript-practices/basics/ex02.js
+++ javascript-practices/basics/ex02.js
@@ -11,13 +11,26 @@
 [객체]
  - object
     1. new 생성자 함수() 사용해서 생성
-    2. 
-    3. 
-    4. 
+        Number() => object type
+        String() => object type
+        Boolean() => object type
+        Object() => object type
+        Array() => object type
+    2. {} 객체 리터럴, JSON 사용 - J(ava)S(cript) O(bject) N(otation)
+    3. [] 배열 리터럴
+    4. null
  - function
+    1. function f() {...} 생성
+    2. var f = function() {...}
+    3. var f = new Function("param1", "param2", ... ,"return ...");
 */
 
-console.log("===기본 타입(premitive, 원시)=====================")
+/*
+자바스크립트 객체를 구분하는 또 다른 방법
+
+*/
+
+console.log("===기본 타입(premitive, 원시)=====================");
 var u = undefined; // var u; 동일. 선언과 정의의 구분이 없다.
 var i = 10;
 var s = "Hello World";
@@ -28,17 +41,48 @@ console.log("i : " + typeof(i));
 console.log("s : " + typeof(s));
 console.log("b : " + typeof(b));
 
-console.log("===객체 타입1 : object type=====================")
+console.log("===객체 타입1 : object type=====================");
 var i2 = new Number(1);
 var s2 = new String("Hello World!@");
 var b2 = new Boolean(false);
 var o = new Object();
 var a = new Array(); // 배열도 객체이다.
 
+var o2 = {};
+var a2 = [];
+
+var n = null; 
+
 console.log("i2 : " + typeof(i2));
 console.log("s2 : " + typeof(s2));
 console.log("b2 : " + typeof(b2));
 console.log("o : " + typeof(o));
 console.log("a : " + typeof(a));
+console.log("o2 : " + typeof(o2));
+console.log("a2 : " + typeof(a2));
+console.log("n : " + typeof(n));
 
-console.log("===객체 타입2 : function type=====================")
+console.log("===객체 타입2 : function type=====================");
+function f1(a, b) {
+    return a + b;
+}
+
+var f2 = function(a, b) {
+    return a + b;
+}
+
+var f3 = new Function("a", "b", "return a + b;");
+
+console.log("f1 : " + typeof(f1));
+console.log("f2 : " + typeof(f2));
+console.log("f3 : " + typeof(f3));
+
+console.log("=== 원시타입과 원시타입의 Wrapper 객체는 구분없이 사용할 수 있다. =====================");
+console.log(i + i2);
+console.log(s + s2);
+
+// 원시 타입에 메서드가 호출될 때, 
+// 임시 Wrapper 객체가 만들어져 메서드 호출이 일어난 뒤 사라진다. (유사 객체)
+console.log("=== 원시타입도 메서드 호출이 가능하다? =====================");
+console.log(b.valueOf()); // JS Engine 안에서 new Boolean(b).valueOf(); 유사 객체로 쓰이고 사라진다.
+console.log(b2.valueOf());
