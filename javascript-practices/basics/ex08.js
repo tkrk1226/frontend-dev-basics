72d5f81f4c5f3fdb156b1ebea86ce26ede81cb4d
UijinJung <tkrk1226@ajou.ac.kr>
2022-02-21 12:51:42 +0900

    js practices [220221-1251]

---
+++ javascript-practices/basics/ex08.js
@@ -0,0 +1,43 @@
+/*
+자바스크립트 객체 3: 확장
+*/
+
+var o = {
+    name: "name",
+    age : 10
+};
+
+var f = function() {
+    console.log('Hello World');
+}
+
+
+console.log(o);
+f();
+
+console.log(" ============ 확장 ============ ");
+o.another = {
+    name : "마이콜",
+    age : 30
+};
+
+f.another = {
+    name : "마이콜",
+    age : 30
+};
+
+console.log(o);
+console.log(f);
+
+console.log(" ============ 기본타입 (primitive type)은 확장되지 않는다. ============ ");
+var i1 = 10;
+var i2 = new Number(10);
+i2.another = {
+    name : "마이콜",
+    age : 30
+};
+console.log(i2);
+
+i1.another = {}; // new Number(i1).another = {}
+console.log(i1.another);
+
